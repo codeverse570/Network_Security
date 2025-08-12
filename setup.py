8b52bd1d15e0cdcd50516e46e8c6c07168ae5071
unknown <patel.36@iitj.ac.in>
2025-08-12 18:40:08 +0530

    created setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
-python-dotenv
-pandas
-numpy
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="network_security_project",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=requirements,
+)
